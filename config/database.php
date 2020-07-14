a1b6978d85941ba321620770c03414a22153b758
Isiko saidiali <isikosaidiali@gmail.com>
2020-07-14 23:26:59 +0300

    init

--- config/database.php
+++ config/database.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Str;
-$url = getenv('JAWSDB_MARIA_URL');
-$dbparts = parse_url($url);
 
 return [
 
@@ -40,7 +38,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => database_path('database.sqlite'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
@@ -48,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => $dbparts['host'],
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => ltrim($dbparts['path'], '/'),
-            'username' => $dbparts['user'],
-            'password' => $dbparts['pass'],
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -125,23 +123,23 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
         ],
 
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', 1),
         ],
 
     ],
--- resources/views/backend/hotel/show.blade.php
+++ resources/views/backend/hotel/show.blade.php
@@ -17,9 +17,90 @@
                                 <li>Spa</li>
                                 <li>Sauna</li>
                                 <li>Escorts</li>
+                                <li>Swimming pool</li>
+                                <li>Free WIFI</li>
+                                <li>Unspeakable security</li>
                             </ol>
                             <h5>Location</h5>
                             <p>Located in {{$hotel->district.' , '.$hotel->address}}</p>
+
+                            <h5>Book here</h5>
+
+                            <div class="row">
+                                <div class="col-lg-8">
+                                    <form action="" class="needs-validation" novalidate>
+                                @csrf
+                                <div class="form-group row">
+                                    <div class="col-lg-4">
+                                        <label for="example-date">Check In</label>
+                                        <input class="form-control" id="example-date" type="date"
+                                            name="date">
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <label for="example-time">Time</label>
+                                            <input class="form-control" id="example-time" type="time" name="time">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-lg-4">
+                                        <label for="example-date">Check Out</label>
+                                        <input class="form-control" id="example-date" type="date"
+                                            name="date">
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <label for="example-time">Time</label>
+                                            <input class="form-control" id="example-time" type="time" name="time">
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="card col-lg-4">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col-md-5">
+                                            <img src="assets/images/small/img-6.jpg" class="card-img" alt="hotel room picture">
+                                            </div>
+                                            <div class="col-md-7">
+                                            <div class="card-body">
+                                                <h5 class="card-title font-size-16">Card title</h5>
+                                                <p class="card-text text-muted">This is a wider card with supporting text lead-in to additional content.</p>
+                                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card col-lg-4">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col-md-5">
+                                            <img src="assets/images/small/img-6.jpg" class="card-img" alt="hotel room picture">
+                                            </div>
+                                            <div class="col-md-7">
+                                            <div class="card-body">
+                                                <h5 class="card-title font-size-16">Card title</h5>
+                                                <p class="card-text text-muted">This is a wider card with supporting text lead-in to additional content.</p>
+                                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card col-lg-4">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col-md-5">
+                                            <img src="assets/images/small/img-6.jpg" class="card-img" alt="hotel room picture">
+                                            </div>
+                                            <div class="col-md-7">
+                                            <div class="card-body">
+                                                <h5 class="card-title font-size-16">Card title</h5>
+                                                <p class="card-text text-muted">This is a wider card with supporting text lead-in to additional content.</p>
+                                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </form>
+                                </div>
+                            </div>
+
                             <h5>Contact</h5>
                             <p>Phone: {{$hotel->phone}}</p>
                             <p>Email: {{$hotel->email}}</p>
