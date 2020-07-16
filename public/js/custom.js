d7f7ee1f405b17970ef0d976910dc55623da1a27
Isiko saidiali <isikosaidiali@gmail.com>
2020-07-16 18:21:11 +0300

    init

--- public/js/custom.js
+++
@@ -1,9 +0,0 @@
-$("#booking_form").submit(function(e) {
-    e.preventDefault()
-    let check_in_date = $("#checkin-date").val()
-    let check_out_date = $("#checkout-date").val()
-    let check_in_time = $("#checkin-time").val()
-    let check_out_time = $("#checkout-time").val()
-    console.log(check_in_date, check_out_date, check_in_time, check_out_time)
-    
-})
