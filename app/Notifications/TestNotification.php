7c46d8ac2be12c8d61fdfe3fa185d1e57bb99303
Isiko saidiali <isikosaidiali@gmail.com>
2020-07-30 10:40:08 +0300

    init

--- app/Notifications/TestNotification.php
+++ app/Notifications/TestNotification.php
@@ -55,7 +55,7 @@ class TestNotification extends Notification
     public function toArray($notifiable)
     {
         return [
-            'message' => 'A new user has registered successfully'
+            // 
         ];
     }
 }
