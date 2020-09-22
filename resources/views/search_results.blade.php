8977d23a4baef6d1863821ea3976fb358b79f183
Isiko saidiali <isikosaidiali@gmail.com>
2020-09-22 22:06:47 +0300

    init

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -1,23 +1,25 @@
 <x-app>
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card p-3 my-5">
+    <div class="row">
+        <div class="col-md-4 my-5">
+            <div class="card">
+                <div class="card-body">
+                    More search options
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 my-5">
+            <div class="card p-3">
                 <div class="card-body">
                     <div class="card-title">
-                        <h3 class="text-primary">{{$results->count()}} Hotels in found {{$search_query}} City</h3>
-                    </div>
-                    <div class="card-text">
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                        </p>
+                        <h3 class="text-primary">{{$results->count()}} Hotels found in {{$search_query}} City</h3>
                     </div>
                 </div>
             </div>
             @forelse ($results as $hotel)
-                <div class="card m-3">
+                <div class="card my-3">
                 <div class="row no-gutters align-items-center">
                     <div class="col-md-5">
-                        <img src="{{$hotel->cover_photos}}" class="card-img" alt="...">
+                        <img src="{{$hotel->cover_photos}}" class="card-img" alt="{{$hotel->name}}">
                         </div>
                         <div class="col-md-7">
                         <div class="card-body">
@@ -62,51 +64,6 @@
             @empty
                 <h3 class="text-center">The search returned empty</h3>
             @endforelse
-            {{-- <div class="card m-3">
-                <div class="row no-gutters align-items-center">
-                    <div class="col-md-5">
-                        <img src="{{asset('images/images/hotels/image18.png')}}" class="card-img" alt="...">
-                        </div>
-                        <div class="col-md-7">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-6">
-                                    <a href="javascript: void(0);"><h5 class="card-title font-size-16">Hotel Name</h5></a>
-                                    <p class="card-text text-muted">Location: <small>Hotel address</small></p>
-                                    <p>Amenities:</p>
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <div class="d-flex">
-                                                <i class="uil uil-restaurant mx-1"></i>
-                                                <i class="uil uil-restaurant mx-1"></i>
-                                                <i class="uil uil-restaurant mx-1"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <h3 class="text-center">$123456789</h3>
-
-                                    <div class="row my-4">
-                                        <div class="col-12">
-                                            <div class="text-center">
-                                                <a href="javascript:void(0);" class="btn btn-outline-primary">
-                                                    <i class="uil uil-sync"></i>
-                                                    Show more
-                                                </a>
-                                            </div>
-                                        </div> <!-- end col-->
-                                    </div>
-                                    <div class="d-flex justify-content-center">
-                                        <x-rating></x-rating>
-                                        <h3>4.5</h3>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
         </div>
     </div>
 </x-app>
